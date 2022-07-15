ef9e4f95999689989dfc4f10df594223e9fc04f3
Nicholas Catalan <107813010+Catalan-Nick@users.noreply.github.com>
2022-07-14 23:44:32 -0500

    Added moves to display

--- display.py
+++ display.py
@@ -1,3 +1,5 @@
+from random import randrange
+
 def poke_info(pokemon):
     
     id_pokemon = pokemon['id']
@@ -19,3 +21,27 @@ def poke_info(pokemon):
         a_name = ability.get('ability')
         print(f"\t{a_name.get('name')}")
         
+    #Moves
+    print(f"Four random {p_name.capitalize()} Moves:")
+    # print(len(pokemon['moves']))
+    # move_entry = pokemon['moves']
+    # print(f"move 0 is: {move_entry[0]}")     
+    # for move in pokemon['moves']:
+    #     move_name = move.get('move')
+    #     print(move_name.get('name'))
+    
+    # get the length of how many moves there are,
+    # use that number to make a random number
+    # print the name of that move
+    moves_length = len(pokemon['moves'])
+    
+    i = 0
+    while(i < 4):
+        random_num = randrange(moves_length - 1)
+        move_entry = pokemon['moves']
+        move_random = move_entry[random_num]
+        move_random_entry = move_random.get('move')
+        move_random_name = move_random_entry.get('name')
+        print(f"\t{move_random_name}")
+        i = i + 1
+    
